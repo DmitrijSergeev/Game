f7f48debd3785f993c244adfcf300a7f97a5f599
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-06-13 17:52:37 +0400

    dimych lessons

---
+++ core/state-manager.js
@@ -0,0 +1,6 @@
+const _state = {
+    points: {
+        google: 12,
+        players: [10, 11]
+    }
+}
---
+++ ui/index.js
@@ -0,0 +1,4 @@
+const rootElement = document.getElementById('root');
+
+rootElement.innerHTML = '';
+rootElement.append(`Player1: ${1}, Player2: ${2}, Google: ${0}`)
