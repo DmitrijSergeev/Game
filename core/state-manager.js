75f01b5251e338717e086355b2da353f40761e7d
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-06-14 10:35:20 +0400

    logic

--- core/state-manager.js
+++ core/state-manager.js
@@ -4,3 +4,10 @@ const _state = {
         players: [10, 11]
     }
 }
+
+export function getGooglePoints (){
+    return _state.points.google
+}
+export function getPlayerPoints (playerNumber){
+    return _state.points.players[playerNumber]
+}
--- ui/index.js
+++ ui/index.js
@@ -1,4 +1,11 @@
+import {getGooglePoints, getPlayerPoints} from "../core/state-manager.js";
+
 const rootElement = document.getElementById('root');
 
 rootElement.innerHTML = '';
-rootElement.append(`Player1: ${1}, Player2: ${2}, Google: ${0}`)
+
+const googlePoints = getGooglePoints();
+const playerOnePoints = getPlayerPoints(0);
+const playerTwoPoints = getPlayerPoints(1);
+
+rootElement.append(`Player1: ${googlePoints}, Player2: ${playerOnePoints}, Google: ${playerTwoPoints}`);
